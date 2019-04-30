b11352f3136f991dce529676b31df6ae89b94d3c
Dave Kinkead <dave@kinkead.com.au>
2019-04-30 18:58:40 +0200

    Add highlighter draft

--- reasons.js
+++ reasons.js
@@ -205,7 +205,7 @@ module.exports = {
   flatten: require('array-flatten'),
   diff: require('array-difference')
 }
-},{"array-difference":10,"array-flatten":11,"array-unique":12}],3:[function(require,module,exports){
+},{"array-difference":11,"array-flatten":12,"array-unique":13}],3:[function(require,module,exports){
 arguments[4][1][0].apply(exports,arguments)
 },{"./Utils":2,"dup":1}],4:[function(require,module,exports){
 //  Reasons.js by Dave Kinkead
@@ -500,7 +500,86 @@ Graph.prototype.isToEdge = function (element) {
     return true
   }
 }
-},{"./element":3,"./utils":8}],5:[function(require,module,exports){
+},{"./element":3,"./utils":9}],5:[function(require,module,exports){
+//  Reasons.js by Dave Kinkead
+//  Copyright 2017-2019 University of Queensland
+//  Available under the MIT license
+
+'use strict'
+
+const Utils = require('./utils')
+const uuid = require('uuid/v4')
+
+let last_mark, icons
+
+module.exports = Highlighter
+
+function Highlighter (elementID) {
+  const dom = document.querySelector(elementID)
+  let text = ''
+  let last_selection
+
+  window.addEventListener('mouseup', (event) => {
+    last_selection = document.getSelection()
+    // text = selection.anchorNode.nodeValue.substring(selection.anchorOffset, selection.focusOffset)
+  })
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key == 'm') {
+      highlight(last_selection)
+    }
+  })
+
+  addIcons()
+  addStyles()
+}
+
+function highlight (selection) {
+  let mark = Utils.buildNode('mark', {id: uuid()})
+  mark.addEventListener('mouseenter', (event) => {
+    last_mark = mark.id
+  })
+
+  mark.addEventListener('mouseout', (event) => {
+  })
+  
+  let range = selection.getRangeAt(0)
+  range.surroundContents(mark)
+}
+
+function addIcons () {
+  icons = icons || Utils.buildNode('div', {id: 'hover-icons'}, {class: 'hide'})
+
+  //  build the icons
+  let i =[
+    { text: 'A', func: function (event) { console.log('Aing ' + last_mark) } },
+    { text: 'B', func: function (event) { console.log('Bing ' + last_mark) } },
+    { text: 'C', func: function (event) { console.log('Cing ' + last_mark) } }
+  ]
+  i.forEach((icon) => icons.appendChild(buildIcon(icon)))
+
+  document.querySelector('body').appendChild(icons)
+}
+
+function buildIcon (opts) {
+  let span = Utils.buildNode('span')
+  span.textContent = opts.text
+  span.addEventListener('click', opts.func)
+  return span
+}
+
+function addStyles () {
+  let css = Utils.buildNode('style')
+  css.textContent = `
+    #hover-icons { position: absolute;  }
+    #hover-icons span { background-color: purple; color: white; line-height: 1; padding: 0.8rem 1rem; border-radius: 50%; }
+    mark { padding: 0.25rem 0; }
+  `
+  document.querySelector('head').appendChild(css)
+}
+
+
+},{"./utils":9,"uuid/v4":17}],6:[function(require,module,exports){
 //  Reasons.js by Dave Kinkead
 //  Copyright 2017-2019 University of Queensland
 //  Available under the MIT license
@@ -557,7 +636,7 @@ Mapper.prototype.render = function (elements) {
 Mapper.prototype.export = function () {
   return this.graph.map(element => element.export())
 }
-},{"./graph":4,"./ui":7,"./view":9}],6:[function(require,module,exports){
+},{"./graph":4,"./ui":8,"./view":10}],7:[function(require,module,exports){
 //  Reasons.js by Dave Kinkead
 //  Copyright 2017-2019 University of Queensland
 //  Available under the MIT license
@@ -566,13 +645,18 @@ Mapper.prototype.export = function () {
  * The Reasons.js API.  This module forms the top level wrapper
  */
 const Mapper = require('./mapper')
+const Highlighter = require('./highlighter')
 
 module.exports = {
   mapper: function (dom) {
     return new Mapper(dom)
+  },
+
+  highlighter: function (dom) {
+    return new Highlighter(dom)
   }
 }
-},{"./mapper":5}],7:[function(require,module,exports){
+},{"./highlighter":5,"./mapper":6}],8:[function(require,module,exports){
 //  Reasons.js by Dave Kinkead
 //  Copyright 2017-2019 University of Queensland
 //  Available under the MIT license
@@ -905,9 +989,9 @@ function isMetaKey (event) {
     Keycode.isEventKey(event, 'ControlRight')
   ) ? true : false
 }
-},{"./graph":4,"./utils":8,"./view":9,"keycode":13}],8:[function(require,module,exports){
+},{"./graph":4,"./utils":9,"./view":10,"keycode":14}],9:[function(require,module,exports){
 arguments[4][2][0].apply(exports,arguments)
-},{"array-difference":10,"array-flatten":11,"array-unique":12,"dup":2}],9:[function(require,module,exports){
+},{"array-difference":11,"array-flatten":12,"array-unique":13,"dup":2}],10:[function(require,module,exports){
 //  Reasons.js by Dave Kinkead
 //  Copyright 2017-2019 University of Queensland
 //  Available under the MIT license
@@ -1205,7 +1289,7 @@ function pointOfIntersection (from, rect, buffer) {
 
   return {x: distance * Math.cos(angle), y: distance * Math.sin(angle)}
 }
-},{"./Element":1,"./utils":8}],10:[function(require,module,exports){
+},{"./Element":1,"./utils":9}],11:[function(require,module,exports){
 (function(global) {
 
 	var indexOf = Array.prototype.indexOf || function(elem) {
@@ -1253,7 +1337,7 @@ function pointOfIntersection (from, rect, buffer) {
 
 }(this));
 
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 'use strict'
 
 /**
@@ -1363,7 +1447,7 @@ function flattenDownDepth (array, result, depth) {
   return result
 }
 
-},{}],12:[function(require,module,exports){
+},{}],13:[function(require,module,exports){
 /*!
  * array-unique <https://github.com/jonschlinkert/array-unique>
  *
@@ -1408,7 +1492,7 @@ module.exports.immutable = function uniqueImmutable(arr) {
   return module.exports(newArr);
 };
 
-},{}],13:[function(require,module,exports){
+},{}],14:[function(require,module,exports){
 // Source: http://jsfiddle.net/vWx8V/
 // http://stackoverflow.com/questions/5603195/full-list-of-javascript-keycodes
 
@@ -1585,5 +1669,98 @@ for (var alias in aliases) {
   codes[alias] = aliases[alias]
 }
 
-},{}]},{},[6])(6)
+},{}],15:[function(require,module,exports){
+/**
+ * Convert array of 16 byte values to UUID string format of the form:
+ * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+ */
+var byteToHex = [];
+for (var i = 0; i < 256; ++i) {
+  byteToHex[i] = (i + 0x100).toString(16).substr(1);
+}
+
+function bytesToUuid(buf, offset) {
+  var i = offset || 0;
+  var bth = byteToHex;
+  // join used to fix memory issue caused by concatenation: https://bugs.chromium.org/p/v8/issues/detail?id=3175#c4
+  return ([bth[buf[i++]], bth[buf[i++]], 
+	bth[buf[i++]], bth[buf[i++]], '-',
+	bth[buf[i++]], bth[buf[i++]], '-',
+	bth[buf[i++]], bth[buf[i++]], '-',
+	bth[buf[i++]], bth[buf[i++]], '-',
+	bth[buf[i++]], bth[buf[i++]],
+	bth[buf[i++]], bth[buf[i++]],
+	bth[buf[i++]], bth[buf[i++]]]).join('');
+}
+
+module.exports = bytesToUuid;
+
+},{}],16:[function(require,module,exports){
+// Unique ID creation requires a high quality random # generator.  In the
+// browser this is a little complicated due to unknown quality of Math.random()
+// and inconsistent support for the `crypto` API.  We do the best we can via
+// feature-detection
+
+// getRandomValues needs to be invoked in a context where "this" is a Crypto
+// implementation. Also, find the complete implementation of crypto on IE11.
+var getRandomValues = (typeof(crypto) != 'undefined' && crypto.getRandomValues && crypto.getRandomValues.bind(crypto)) ||
+                      (typeof(msCrypto) != 'undefined' && typeof window.msCrypto.getRandomValues == 'function' && msCrypto.getRandomValues.bind(msCrypto));
+
+if (getRandomValues) {
+  // WHATWG crypto RNG - http://wiki.whatwg.org/wiki/Crypto
+  var rnds8 = new Uint8Array(16); // eslint-disable-line no-undef
+
+  module.exports = function whatwgRNG() {
+    getRandomValues(rnds8);
+    return rnds8;
+  };
+} else {
+  // Math.random()-based (RNG)
+  //
+  // If all else fails, use Math.random().  It's fast, but is of unspecified
+  // quality.
+  var rnds = new Array(16);
+
+  module.exports = function mathRNG() {
+    for (var i = 0, r; i < 16; i++) {
+      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
+      rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
+    }
+
+    return rnds;
+  };
+}
+
+},{}],17:[function(require,module,exports){
+var rng = require('./lib/rng');
+var bytesToUuid = require('./lib/bytesToUuid');
+
+function v4(options, buf, offset) {
+  var i = buf && offset || 0;
+
+  if (typeof(options) == 'string') {
+    buf = options === 'binary' ? new Array(16) : null;
+    options = null;
+  }
+  options = options || {};
+
+  var rnds = options.random || (options.rng || rng)();
+
+  // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+  rnds[6] = (rnds[6] & 0x0f) | 0x40;
+  rnds[8] = (rnds[8] & 0x3f) | 0x80;
+
+  // Copy bytes to buffer, if provided
+  if (buf) {
+    for (var ii = 0; ii < 16; ++ii) {
+      buf[i + ii] = rnds[ii];
+    }
+  }
+
+  return buf || bytesToUuid(rnds);
+}
+
+module.exports = v4;
+
+},{"./lib/bytesToUuid":15,"./lib/rng":16}]},{},[7])(7)
 });
