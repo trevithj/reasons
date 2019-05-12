c5d02a7bea65148f5eef5523effada4878ff9acc
Dave Kinkead <dave@kinkead.com.au>
2019-05-12 13:59:56 +0200

    Stash highlighter

--- reasons.js
+++ reasons.js
@@ -510,23 +510,32 @@ Graph.prototype.isToEdge = function (element) {
 const Utils = require('./utils')
 const uuid = require('uuid/v4')
 
-let last_mark, icons
+let last_mark, 
+    last_range,
+    reasons = [],
+    icons = Utils.buildNode('div', {id: 'hover-icons'}, {class: 'hide'})
+
 
 module.exports = Highlighter
 
 function Highlighter (elementID) {
   const dom = document.querySelector(elementID)
   let text = ''
-  let last_selection
+  // let reasons = window.localStorage.getItem('reasons')
+  // console.log(reasons)
+  // reasons.forEach((reason) => {
+  //   Document.createRange(reason)
+  // })
 
   window.addEventListener('mouseup', (event) => {
-    last_selection = document.getSelection()
-    // text = selection.anchorNode.nodeValue.substring(selection.anchorOffset, selection.focusOffset)
-  })
+    let width = (icons.getClientRects().length) ? icons.getClientRects()[0].width : 50
+    let new_selection = document.getSelection()
 
-  window.addEventListener('keydown', (event) => {
-    if (event.key == 'm') {
-      highlight(last_selection)
+    if (new_selection && new_selection.focusOffset > new_selection.anchorOffset) {
+      last_range = new_selection.getRangeAt(0).cloneRange()
+      let coords = getXandY(new_selection)
+      icons.classList.remove('hide')
+      icons.setAttribute('style', `top: ${coords.y - 40}px; left: ${coords.x - width/2}px;`)
     }
   })
 
@@ -534,35 +543,55 @@ function Highlighter (elementID) {
   addStyles()
 }
 
-function highlight (selection) {
+function getXandY (selection) {
+  let minX, maxX, minY
+
+  let boxes = selection.getRangeAt(0).cloneRange().getClientRects()
+  for (let i=0; i<boxes.length; i++) {
+    let box = boxes[i]
+    minX = Math.min((minX || box.left), box.left)
+    maxX = Math.max((maxX || box.right), box.right)
+    minY = Math.min((minY || box.top), box.top)
+  }
+  return { x: (maxX - minX)/2 + minX, y: minY }
+}
+
+function highlight (range) {
+
   let mark = Utils.buildNode('mark', {id: uuid()})
   mark.addEventListener('mouseenter', (event) => {
     last_mark = mark.id
   })
 
   mark.addEventListener('mouseout', (event) => {
+    icons.classList.add('hide')
   })
-  
-  let range = selection.getRangeAt(0)
+
+  save(range)  
   range.surroundContents(mark)
 }
 
-function addIcons () {
-  icons = icons || Utils.buildNode('div', {id: 'hover-icons'}, {class: 'hide'})
+function save (range) {
+  console.log(range)
+  reasons.push(range)
+  window.localStorage.setItem('reasons', reasons)
+}
 
+function addIcons () {
   //  build the icons
-  let i =[
-    { text: 'A', func: function (event) { console.log('Aing ' + last_mark) } },
-    { text: 'B', func: function (event) { console.log('Bing ' + last_mark) } },
-    { text: 'C', func: function (event) { console.log('Cing ' + last_mark) } }
-  ]
-  i.forEach((icon) => icons.appendChild(buildIcon(icon)))
+  let saveIcon = Utils.buildNode('span', { title: 'Save highlight as a reason' })
+  saveIcon.textContent = '+'
+  saveIcon.addEventListener('click', function (event) {
+    icons.classList.add('hide')
+    highlight(last_range)
+  })
+  icons.appendChild(saveIcon)
 
   document.querySelector('body').appendChild(icons)
 }
 
 function buildIcon (opts) {
-  let span = Utils.buildNode('span')
+  let span = Utils.buildNode('span', opts.opts, opts.attribs)
   span.textContent = opts.text
   span.addEventListener('click', opts.func)
   return span
@@ -571,9 +600,10 @@ function buildIcon (opts) {
 function addStyles () {
   let css = Utils.buildNode('style')
   css.textContent = `
-    #hover-icons { position: absolute;  }
-    #hover-icons span { background-color: purple; color: white; line-height: 1; padding: 0.8rem 1rem; border-radius: 50%; }
+    #hover-icons { position: absolute; }
+    #hover-icons span { background-color: rgba(128,0,128,0.7); color: white; line-height: 1; padding: 0.8rem 1rem; border-radius: 50%; }
     mark { padding: 0.25rem 0; }
+    .hide { display: none; transition: display 2s; }
   `
   document.querySelector('head').appendChild(css)
 }
